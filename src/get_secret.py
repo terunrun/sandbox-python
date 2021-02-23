4326c616d448c40451716394f473f73c45ad2988
terunrun <terunrun@gmail.com>
2021-02-23 15:47:02 +0900

    get json formatted secret

--- src/get_secret.py
+++ src/get_secret.py
@@ -1,14 +1,29 @@
 """Secret Manager から情報を取得する"""
 
 from google.cloud import secretmanager
+import ast
 import consts
 
-SECRET_NAME = 'test_secret'
-SECRET_VARSION = '1'
+SECRET_NAME = os.environ.get('SECRET_NAME')
+SECRET_VARSION = os.environ.get('SECRET_VARSION')
 
 client = secretmanager.SecretManagerServiceClient()
 name = client.secret_version_path(consts.PROJECT_ID, SECRET_NAME, SECRET_VARSION)
 print('target secret access path is {}'.format(name))
-response = client.access_secret_version(request={"name": name})
+response = client.access_secret_version(request={'name': name})
+data = response.payload.data.decode('UTF-8')
 
-print('secret is {}'.format(response.payload.data.decode('UTF-8')))
+try:
+    # 取得したSecretは文字列であるため、辞書に持ち換える
+    dic = ast.literal_eval(data)
+    for key, value in dic.items():
+        print('key is {}, value is {}'.format(key, value))
+    print('value_1 is {}'.format(dic['key1']))
+    print('value_2 is {}'.format(dic['key2']))
+    print('value_3 is {}'.format(dic['key3']))
+except ValueError:
+    # Secretが辞書形式でない場合は、文字列をそのまま表示する
+    print('Secret is not dictionary!')
+    print('Secret value is {}'.format(data))
+except Exception:
+    print('Can not convert because unexpected error!')
